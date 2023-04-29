56b5082fcb59f81c8a478d8c66120cea17072d87
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-04-28 19:56:30 -0600

    Trabajando en componenete card

--- src/components/Back_Card.jsx
+++ src/components/Back_Card.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Back_Card = () => {
     return (
         <div className='card1'>
+        <input type='text' className='input-back-card' readOnly></input>
         <img src='../../public/assets/images/bg-card-back.png' alt='card'></img>
         </div>
     )
--- src/components/Front_Card.jsx
+++ src/components/Front_Card.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const Front_Card = () => {
   return (
     <div className='card2'>
+      <span className='span-front-card-1'>JANE APPLESEED</span>
+      <span className='span-front-card-2'>00/00</span>
+      <img src='../public/assets/images/card-logo.svg' className='icon-card'></img>
+      <input type='text' readOnly className='input-front-card'></input>
       <img src='../public/assets/images/bg-card-front.png' alt='card'></img>
     </div>
   )
