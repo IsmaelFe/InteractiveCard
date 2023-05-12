fee4d1b5d532239a48950ff6e3ffdcb26bab306c
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-05-12 15:14:47 -0600

    Error en las imagenes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,8 +14,8 @@ const Main = () => {
   };
 
   let completeState = false;
-  let mobileImage = "./public/assets/images/bg-main-mobile.png";
-  let desktopImage = "./public/assets/images/bg-main-desktop.png";
+  let mobileImage = "../../images/bg-main-mobile.png";
+  let desktopImage = "../../public/assets/images/bg-main-desktop.png";
 
   const [card, setCard] = useState(initialState);
   const [image, setImage] = useState(window.innerWidth);
