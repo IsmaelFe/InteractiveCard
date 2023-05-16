24aceab3e418347224bbcecda3dbd4ddc344f6a7
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-05-15 18:18:41 -0600

    Nueva versión

--- src/components/Front_Card.jsx
+++ src/components/Front_Card.jsx
@@ -2,24 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Front_Card = ({ card, image }) => {
-  const stylesInput = {
-    color: "white",
-    fontSize: "1.3em",
-    textAlign: "center",
-    letterSpacing: "2px",
-    fontWeight: "100",
-  };
-
-  const stylesDesktop = {
-    width: "330px",
-    color: "white",
-    fontSize: "1.5em",
-    textAlign: "center",
-    letterSpacing: "3px",
-    fontWeight: "100",
-    marginLeft: "30px",
-  };
-
   let value = card.number;
   let numberOne = value.substr(0, 4);
   let numberTwo = value.substr(4, 4);
@@ -39,7 +21,6 @@ const Front_Card = ({ card, image }) => {
       </span>
       <img src="../../assets/images/card-logo.svg" className="icon-card"></img>
       <input
-        style={image > 500 ? stylesDesktop : stylesInput}
         value={`${numberOne} ${numberTwo} ${numberTre} ${numberFour}`}
         type="text"
         readOnly
